69b0fca4c6422f0205eb9b76ea4c0a12790e36d4
xiaojianwu <dd4x@qq.com>
2016-10-31 17:01:46 +0800

    dso抽取

---
+++ libOEF/libOEF/dsoframer.cpp
@@ -0,0 +1,378 @@
+/***************************************************************************
+ * DSOFCONTROL.CPP
+ *
+ * CDsoFramerControl: The Base Control
+ *
+ *  Copyright ?999-2004; Microsoft Corporation. All rights reserved.
+ *  Written by Microsoft Developer Support Office Integration (PSS DSOI)
+ * 
+ *  This code is provided via KB 311765 as a sample. It is not a formal
+ *  product and has not been tested with all containers or servers. Use it
+ *  for educational purposes only. See the EULA.TXT file included in the
+ *  KB download for full terms of use and restrictions.
+ *
+ *  THIS CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ ***************************************************************************/
+#include "dsoframer.h"
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameHookManager
+//
+CDsoFrameHookManager* vgGlobalManager = NULL;
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameHookManager::RegisterFramerControl
+//
+//  Creates a frame hook and registers it with the active manager. If 
+//  this is the first time called, the global manager will be created
+//  now.  The manager can handle up to DSOF_MAX_CONTROLS controls (10).
+//
+STDMETHODIMP_(CDsoFrameHookManager*) CDsoFrameHookManager::RegisterFramerControl(HWND hwndParent, HWND hwndControl)
+{
+	CDsoFrameHookManager* pManager = NULL;
+	ODS("CDsoFrameHookManager::RegisterFramerControl\n");
+    CHECK_NULL_RETURN(hwndControl, NULL);
+
+	EnterCriticalSection(&v_csecThreadSynch);
+	pManager = vgGlobalManager;
+	if (pManager == NULL)
+		pManager = (vgGlobalManager = new CDsoFrameHookManager());
+	LeaveCriticalSection(&v_csecThreadSynch);
+
+	if (pManager)
+	{
+        if (hwndParent == NULL)
+            hwndParent = hwndControl;
+
+		if (FAILED(pManager->AddComponent(hwndParent, hwndControl)))
+			return NULL;
+	}
+
+	return pManager;
+}
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameHookManager::AddComponent
+//
+//  Register the component and create the actual frame hook (if needed).
+//
+STDMETHODIMP CDsoFrameHookManager::AddComponent(HWND hwndParent, HWND hwndControl)
+{
+	DWORD dwHostProcessID;
+	CDsoFrameWindowHook* phook;
+	HRESULT hr = E_OUTOFMEMORY;
+	HWND hwndNext, hwndHost = hwndParent;
+	ODS("CDsoFrameHookManager::AddComponent\n");
+
+ // Find the top-level window this control is sited on...
+	while (hwndNext = GetParent(hwndHost))
+		hwndHost = hwndNext;
+
+ // We have to get valid window that is in-thread for a subclass to work...
+	if (!IsWindow(hwndHost) || 
+		(GetWindowThreadProcessId(hwndHost, &dwHostProcessID), 
+			dwHostProcessID != GetCurrentProcessId()))
+		return E_ACCESSDENIED;
+
+ // Next if we have room in the array, add the component to the list and
+ // hook its parent window so we get messages for it.
+	EnterCriticalSection(&v_csecThreadSynch);
+	if ((vgGlobalManager) && (m_cComponents < DSOF_MAX_CONTROLS))
+	{
+	 // Hook the host window for this control...
+		phook = CDsoFrameWindowHook::AttachToFrameWindow(hwndHost);
+		if (phook) 
+		{// Add the component to the list if hooked...
+			m_pComponents[m_cComponents].hwndControl = hwndControl;
+			m_pComponents[m_cComponents].phookFrame  = phook;
+			m_cComponents++; hr = S_OK; 
+		}
+	}
+	LeaveCriticalSection(&v_csecThreadSynch);
+
+	if (SUCCEEDED(hr)) // Make sure item is the new active component
+		hr = SetActiveComponent(hwndControl);
+
+	return hr;
+}
+
+STDMETHODIMP CDsoFrameHookManager::DetachComponent(HWND hwndControl)
+{
+	DWORD dwIndex;
+	CHECK_NULL_RETURN(m_cComponents, E_FAIL);
+
+ // Find this control in the list...
+	for (dwIndex = 0; dwIndex < m_cComponents; dwIndex++)
+		if (m_pComponents[dwIndex].hwndControl == hwndControl) break;
+
+	if (dwIndex > m_cComponents)
+		return E_INVALIDARG;
+
+ // If we found the index, remove the item and shift remaining
+ // items down the list. Change active index as needed...
+	EnterCriticalSection(&v_csecThreadSynch);
+
+	m_pComponents[dwIndex].hwndControl = NULL;
+	m_pComponents[dwIndex].phookFrame->Detach();
+
+ // Compact the list...
+	while (++dwIndex < m_cComponents)
+	{
+		m_pComponents[dwIndex-1].hwndControl = m_pComponents[dwIndex].hwndControl;
+		m_pComponents[dwIndex-1].phookFrame = m_pComponents[dwIndex].phookFrame;
+		m_pComponents[dwIndex].hwndControl = NULL;
+	}
+
+ // Decrement the count. If count still exists, forward activation
+ // to next component in the list...
+	if (--m_cComponents)
+	{
+		while (m_idxActive >= m_cComponents)
+			--m_idxActive;
+
+		HWND hwndActive = GetActiveComponentWindow();
+		if (hwndActive) 
+			SendNotifyMessage(hwndActive, DSO_WM_HOOK_NOTIFY_COMPACTIVE, (WPARAM)TRUE, (LPARAM)(m_fAppActive));
+	}
+	else
+	{
+		vgGlobalManager = NULL;
+	 // If this is the last control, we can remove the manger!
+		delete this;
+	}
+
+	LeaveCriticalSection(&v_csecThreadSynch);
+
+	return S_OK;
+}
+
+STDMETHODIMP CDsoFrameHookManager::SetActiveComponent(HWND hwndControl)
+{
+	ODS(" -- CDsoFrameHookManager::SetActiveComponent -- \n");
+	CHECK_NULL_RETURN(m_cComponents, E_FAIL);
+	DWORD dwIndex;
+
+ // Find the index of the control...
+	for (dwIndex = 0; dwIndex < m_cComponents; dwIndex++)
+		if (m_pComponents[dwIndex].hwndControl == hwndControl) break;
+
+	if (dwIndex > m_cComponents)
+		return E_INVALIDARG;
+
+ // If it is not already the active item, notify old component it is
+ // losing activation, and notify new component that it is gaining...
+	EnterCriticalSection(&v_csecThreadSynch);
+	if (dwIndex != m_idxActive)
+	{
+		HWND hwndActive = GetActiveComponentWindow();
+		if (hwndActive) 
+			SendNotifyMessage(hwndActive, DSO_WM_HOOK_NOTIFY_COMPACTIVE, (WPARAM)FALSE, (LPARAM)0);
+
+		m_idxActive = dwIndex;
+
+		hwndActive = GetActiveComponentWindow();
+		if (hwndActive)
+			SendNotifyMessage(hwndActive, DSO_WM_HOOK_NOTIFY_COMPACTIVE, (WPARAM)TRUE, (LPARAM)(m_fAppActive));
+	}
+	LeaveCriticalSection(&v_csecThreadSynch);
+
+	return S_OK;
+}
+
+STDMETHODIMP CDsoFrameHookManager::OnComponentNotify(DWORD msg, WPARAM wParam, LPARAM lParam)
+{
+	TRACE3("CDsoFrameHookManager::OnComponentNotify(%d, %d, %d)\n", msg, wParam, lParam);
+	HWND hwndActiveComp = GetActiveComponentWindow();
+	
+ // The manager needs to keep track of AppActive state...
+	if (msg == DSO_WM_HOOK_NOTIFY_APPACTIVATE)
+	{
+		EnterCriticalSection(&v_csecThreadSynch);
+		m_fAppActive = (BOOL)wParam;
+		LeaveCriticalSection(&v_csecThreadSynch);
+	}
+
+ // Send the message to the active component
+    SendNotifyMessage(hwndActiveComp, msg, wParam, lParam);
+	return S_OK;
+}
+
+STDMETHODIMP_(BOOL) CDsoFrameHookManager::SendNotifyMessage(HWND hwnd, DWORD msg, WPARAM wParam, LPARAM lParam)
+{
+    BOOL fResult;
+
+ // Check if the caller is on the same thread as the active component. If so we can
+ // forward the message directly for processing. If not, we have to post and wait for
+ // that thread to process message at a later time...
+	if (GetWindowThreadProcessId(hwnd, NULL) == GetCurrentThreadId())
+	{ // Notify now...
+		fResult = (BOOL)SendMessage(hwnd, msg, wParam, lParam);
+	}
+	else
+	{ // Notify later...
+		fResult = PostMessage(hwnd, msg, wParam, lParam);
+	}
+
+    return fResult;
+}
+
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameWindowHook
+//
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameWindowHook::AttachToFrameWindow
+//
+//  Creates the frame hook for the host window.
+//
+STDMETHODIMP_(CDsoFrameWindowHook*) CDsoFrameWindowHook::AttachToFrameWindow(HWND hwndParent)
+{
+	CDsoFrameWindowHook* phook = NULL;
+	BOOL fHookSuccess = FALSE;
+
+ // We have to get valid window ...
+	if (!IsWindow(hwndParent))
+		return NULL;
+
+	EnterCriticalSection(&v_csecThreadSynch);
+	phook = CDsoFrameWindowHook::GetHookFromWindow(hwndParent);
+	if (phook)
+	{ // Already have this window hooked, so just addref the count.
+		phook->AddRef();
+	}
+	else
+	{ // Need to create a new hook object for this window...
+		if ((phook = new CDsoFrameWindowHook()))
+		{
+			phook->m_hwndTopLevelHost = hwndParent;
+			phook->m_fHostUnicodeWindow = (v_fUnicodeAPI && IsWindowUnicode(hwndParent));
+
+		 // Setup the subclass using Unicode or Ansi methods depending on window 
+		 // create state. Note if either property assoc or window subclass fail...
+			if (phook->m_fHostUnicodeWindow)
+			{
+				fHookSuccess = SetPropW(hwndParent, L"DSOFramerWndHook", (HANDLE)phook);
+				if (fHookSuccess)
+				{
+					phook->m_pfnOrigWndProc = (WNDPROC)SetWindowLongW(hwndParent, GWL_WNDPROC, (LONG)(WNDPROC)HostWindowProcHook);
+					fHookSuccess = (phook->m_pfnOrigWndProc != NULL);
+				}
+			}
+			else
+			{
+				fHookSuccess = SetPropA(hwndParent, "DSOFramerWndHook", (HANDLE)phook);
+				if (fHookSuccess)
+				{
+					phook->m_pfnOrigWndProc = (WNDPROC)SetWindowLongA(hwndParent, GWL_WNDPROC, (LONG)(WNDPROC)HostWindowProcHook);
+					fHookSuccess = (phook->m_pfnOrigWndProc != NULL);
+				}
+			}
+
+		 // If we failed the subclass, kill this object and return error...
+			if (!fHookSuccess)
+			{
+				delete phook;
+				phook = NULL;
+			}
+			else phook->m_cHookCount++;
+		}
+	}
+	LeaveCriticalSection(&v_csecThreadSynch);
+
+	return phook;
+}
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameWindowHook::Detach
+//
+//  Removes the hook from the frame window if this is the last control
+//  to reference the hook.
+//
+STDMETHODIMP CDsoFrameWindowHook::Detach()
+{
+ // Only need to call this if item is attached.
+	EnterCriticalSection(&v_csecThreadSynch);
+	if (m_cHookCount)
+	{
+		if (--m_cHookCount == 0)
+		{ // We can remove the hook!
+			if (m_fHostUnicodeWindow)
+			{
+				SetWindowLongW(m_hwndTopLevelHost, GWL_WNDPROC, (LONG)(m_pfnOrigWndProc));
+				RemovePropW(m_hwndTopLevelHost, L"DSOFramerWndHook");
+			}
+			else
+			{
+				SetWindowLongA(m_hwndTopLevelHost, GWL_WNDPROC, (LONG)(m_pfnOrigWndProc));
+				RemovePropA(m_hwndTopLevelHost, "DSOFramerWndHook");
+			}
+			m_hwndTopLevelHost = NULL;
+
+		// we can kill this object now...
+			delete this;
+		}
+	}
+	LeaveCriticalSection(&v_csecThreadSynch);
+	return S_OK;
+}
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameWindowHook::GetHookFromWindow
+//
+//  Returns the hook control associated with this object.
+//
+STDMETHODIMP_(CDsoFrameWindowHook*) CDsoFrameWindowHook::GetHookFromWindow(HWND hwnd)
+{
+	CDsoFrameWindowHook* phook = (CDsoFrameWindowHook*)GetPropW(hwnd, L"DSOFramerWndHook");
+	if (phook == NULL) phook = (CDsoFrameWindowHook*)GetPropA(hwnd, "DSOFramerWndHook");
+	return phook;
+}
+
+////////////////////////////////////////////////////////////////////////
+// CDsoFrameWindowHook::HostWindowProcHook
+//
+//  The window proc for the subclassed host window.
+//
+STDMETHODIMP_(LRESULT) CDsoFrameWindowHook::HostWindowProcHook(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	CDsoFrameWindowHook* phook = CDsoFrameWindowHook::GetHookFromWindow(hwnd);
+	if (phook) // We better get our prop for this window!
+	{
+	 // We handle the important ActiveDocument frame-level events like task 
+	 // window activation/deactivation, palette change notfication, and key/mouse
+	 // focus notification on host app focus...
+		switch (msg)
+		{
+		case WM_ACTIVATEAPP:
+			if (vgGlobalManager)
+				vgGlobalManager->OnComponentNotify(DSO_WM_HOOK_NOTIFY_APPACTIVATE, wParam, lParam);
+			break;
+
+		case WM_PALETTECHANGED:
+			if (vgGlobalManager)
+				vgGlobalManager->OnComponentNotify(DSO_WM_HOOK_NOTIFY_PALETTECHANGE, wParam, lParam);
+			break;
+
+        case WM_SYNCPAINT:
+			if (vgGlobalManager)
+				vgGlobalManager->OnComponentNotify(DSO_WM_HOOK_NOTIFY_SYNCPAINT, wParam, lParam);
+			break;
+
+		}
+
+	 // After processing, allow calls to fall through to host app. We need to call
+	 // the appropriate handler for Unicode or non-Unicode windows...
+		if (phook->m_fHostUnicodeWindow)
+			return CallWindowProcW(phook->m_pfnOrigWndProc, hwnd, msg, wParam, lParam);
+		else
+			return CallWindowProcA(phook->m_pfnOrigWndProc, hwnd, msg, wParam, lParam);
+	}
+
+ // Should not be reached, but just in case call default proc...
+	return (IsWindowUnicode(hwnd) ? 
+			DefWindowProcW(hwnd, msg, wParam, lParam) : 
+			DefWindowProcA(hwnd, msg, wParam, lParam));
+}
---
+++ libOEF/libOEF/dsoframer.h
@@ -0,0 +1,202 @@
+/***************************************************************************
+ * DSOFRAMER.H
+ *
+ * Developer Support Office ActiveX Document Framer Control Sample
+ *
+ *  Copyright ?999-2004; Microsoft Corporation. All rights reserved.
+ *  Written by Microsoft Developer Support Office Integration (PSS DSOI)
+ * 
+ *  This code is provided via KB 311765 as a sample. It is not a formal
+ *  product and has not been tested with all containers or servers. Use it
+ *  for educational purposes only.
+ *
+ *  You have a royalty-free right to use, modify, reproduce and distribute
+ *  this sample application, and/or any modified version, in any way you
+ *  find useful, provided that you agree that Microsoft has no warranty,
+ *  obligations or liability for the code or information provided herein.
+ *
+ *  THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  See the EULA.TXT file included in the KB download for full terms of use
+ *  and restrictions. You should consult documentation on MSDN Library for
+ *  possible updates or changes to behaviors or interfaces used in this sample.
+ *
+ ***************************************************************************/
+#ifndef DS_DSOFRAMER_H 
+#define DS_DSOFRAMER_H
+
+////////////////////////////////////////////////////////////////////
+// We compile at level 4 and disable some unnecessary warnings...
+//
+#pragma warning(push, 4) // Compile at level-4 warnings
+#pragma warning(disable: 4100) // unreferenced formal parameter (in OLE this is common)
+#pragma warning(disable: 4146) // unary minus operator applied to unsigned type, result still unsigned
+#pragma warning(disable: 4268) // const static/global data initialized with compiler generated default constructor
+#pragma warning(disable: 4310) // cast truncates constant value
+#pragma warning(disable: 4786) // identifier was truncated in the debug information
+
+////////////////////////////////////////////////////////////////////
+// Compile Options For Modified Behavior...
+//
+#define DSO_MSDAIPP_USE_DAVONLY          // Default to WebDAV protocol for open by HTTP/HTTPS
+#define DSO_WORD12_PERSIST_BUG           // Perform workaround for IPersistFile bug in Word 2007
+
+////////////////////////////////////////////////////////////////////
+// Needed include files (both standard and custom)
+//
+#include <windows.h>
+#include <ole2.h>
+#include <olectl.h>
+#include <oleidl.h>
+#include <objsafe.h>
+
+#include "utilities.h"
+#include "dsofdocobj.h"
+
+////////////////////////////////////////////////////////////////////
+// Global Variables
+//
+extern HINSTANCE        v_hModule;
+extern CRITICAL_SECTION v_csecThreadSynch;
+extern HICON            v_icoOffDocIcon;
+extern ULONG            v_cLocks;
+extern BOOL             v_fUnicodeAPI;
+extern BOOL             v_fWindows2KPlus;
+
+////////////////////////////////////////////////////////////////////
+// Custom Errors - we support a very limited set of custom error messages
+//
+#define DSO_E_ERR_BASE              0x80041100
+#define DSO_E_UNKNOWN               0x80041101   // "An unknown problem has occurred."
+#define DSO_E_INVALIDPROGID         0x80041102   // "The ProgID/Template could not be found or is not associated with a COM server."
+#define DSO_E_INVALIDSERVER         0x80041103   // "The associated COM server does not support ActiveX Document embedding."
+#define DSO_E_COMMANDNOTSUPPORTED   0x80041104   // "The command is not supported by the document server."
+#define DSO_E_DOCUMENTREADONLY      0x80041105   // "Unable to perform action because document was opened in read-only mode."
+#define DSO_E_REQUIRESMSDAIPP       0x80041106   // "The Microsoft Internet Publishing Provider is not installed, so the URL document cannot be open for write access."
+#define DSO_E_DOCUMENTNOTOPEN       0x80041107   // "No document is open to perform the operation requested."
+#define DSO_E_INMODALSTATE          0x80041108   // "Cannot access document when in modal condition."
+#define DSO_E_NOTBEENSAVED          0x80041109   // "Cannot Save file without a file path."
+#define DSO_E_FRAMEHOOKFAILED       0x8004110A   // "Unable to set frame hook for the parent window."
+#define DSO_E_ERR_MAX               0x8004110B
+
+////////////////////////////////////////////////////////////////////
+// Custom OLE Command IDs - we use for special tasks
+//
+#define OLECMDID_GETDATAFORMAT      0x7001  // 28673
+#define OLECMDID_SETDATAFORMAT      0x7002  // 28674
+#define OLECMDID_LOCKSERVER         0x7003  // 28675
+#define OLECMDID_RESETFRAMEHOOK     0x7009  // 28681
+#define OLECMDID_NOTIFYACTIVE       0x700A  // 28682
+
+////////////////////////////////////////////////////////////////////
+// Custom Window Messages (only apply to CDsoFramerControl window proc)
+//
+#define DSO_WM_ASYNCH_OLECOMMAND         (WM_USER + 300)
+#define DSO_WM_ASYNCH_STATECHANGE        (WM_USER + 301)
+
+#define DSO_WM_HOOK_NOTIFY_COMPACTIVE    (WM_USER + 400)
+#define DSO_WM_HOOK_NOTIFY_APPACTIVATE   (WM_USER + 401)
+#define DSO_WM_HOOK_NOTIFY_FOCUSCHANGE   (WM_USER + 402)
+#define DSO_WM_HOOK_NOTIFY_SYNCPAINT     (WM_USER + 403)
+#define DSO_WM_HOOK_NOTIFY_PALETTECHANGE (WM_USER + 404)
+
+// State Flags for DSO_WM_ASYNCH_STATECHANGE:
+#define DSO_STATE_MODAL            1
+#define DSO_STATE_ACTIVATION       2
+#define DSO_STATE_INTERACTIVE      3
+#define DSO_STATE_RETURNFROMMODAL  4
+
+
+////////////////////////////////////////////////////////////////////
+// Menu Bar Items
+//
+#define DSO_MAX_MENUITEMS         16
+#define DSO_MAX_MENUNAME_LENGTH   32
+
+#ifndef DT_HIDEPREFIX
+#define DT_HIDEPREFIX             0x00100000
+#define DT_PREFIXONLY             0x00200000
+#endif
+
+#define SYNCPAINT_TIMER_ID         4
+
+
+////////////////////////////////////////////////////////////////////
+// CDsoFrameWindowHook -- Frame Window Hook Class
+//
+//  Used by the control to allow for proper host notification of focus 
+//  and activation events occurring at top-level window frame. Because 
+//  this DocObject host is an OCX, we don't own these notifications and
+//  have to "steal" them from our parent using a subclass.
+//
+//  IMPORTANT: Since the parent frame may exist on a separate thread, this
+//  class does nothing but the hook. The code to notify the active component
+//  is in a separate global class that is shared by all threads.
+//
+class CDsoFrameWindowHook
+{
+public:
+	CDsoFrameWindowHook(){ODS("CDsoFrameWindowHook created\n");m_cHookCount=0;m_hwndTopLevelHost=NULL;m_pfnOrigWndProc=NULL;m_fHostUnicodeWindow=FALSE;}
+	~CDsoFrameWindowHook(){ODS("CDsoFrameWindowHook deleted\n");}
+
+	static STDMETHODIMP_(CDsoFrameWindowHook*) AttachToFrameWindow(HWND hwndParent);
+	STDMETHODIMP Detach();
+
+	static STDMETHODIMP_(CDsoFrameWindowHook*) GetHookFromWindow(HWND hwnd);
+	inline STDMETHODIMP_(void) AddRef(){InterlockedIncrement((LONG*)&m_cHookCount);}
+
+    static STDMETHODIMP_(LRESULT) 
+		HostWindowProcHook(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+	DWORD                   m_cHookCount;
+	HWND                    m_hwndTopLevelHost;    // Top-level host window (hooked)
+    WNDPROC                 m_pfnOrigWndProc;
+	BOOL                    m_fHostUnicodeWindow;
+};
+
+// THE MAX NUMBER OF DSOFRAMER CONTROLS PER PROCESS
+#define DSOF_MAX_CONTROLS   10
+
+////////////////////////////////////////////////////////////////////
+// CDsoFrameHookManager -- Hook Manager Class
+//
+//  Used to keep track of which control is active and forward notifications
+//  to it using window messages (to cross thread boundaries).  
+//
+class CDsoFrameHookManager
+{
+public:
+	CDsoFrameHookManager(){ODS("CDsoFrameHookManager created\n"); m_fAppActive=TRUE; m_idxActive=DSOF_MAX_CONTROLS; m_cComponents=0;}
+	~CDsoFrameHookManager(){ODS("CDsoFrameHookManager deleted\n");}
+
+	static STDMETHODIMP_(CDsoFrameHookManager*)
+		RegisterFramerControl(HWND hwndParent, HWND hwndControl);
+
+	STDMETHODIMP AddComponent(HWND hwndParent, HWND hwndControl);
+	STDMETHODIMP DetachComponent(HWND hwndControl);
+	STDMETHODIMP SetActiveComponent(HWND hwndControl);
+	STDMETHODIMP OnComponentNotify(DWORD msg, WPARAM wParam, LPARAM lParam);
+
+	inline STDMETHODIMP_(HWND)
+		GetActiveComponentWindow(){return m_pComponents[m_idxActive].hwndControl;}
+
+	inline STDMETHODIMP_(CDsoFrameWindowHook*)
+		GetActiveComponentFrame(){return m_pComponents[m_idxActive].phookFrame;}
+
+	STDMETHODIMP_(BOOL) SendNotifyMessage(HWND hwnd, DWORD msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+	BOOL                    m_fAppActive;
+	DWORD                   m_idxActive;
+	DWORD                   m_cComponents;
+    struct FHOOK_COMPONENTS
+	{
+		HWND hwndControl;
+		CDsoFrameWindowHook *phookFrame;
+	}                       m_pComponents[DSOF_MAX_CONTROLS];
+};
+
+#endif //DS_DSOFRAMER_H
